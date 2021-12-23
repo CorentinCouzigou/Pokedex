8ecb063fdc4e697144a3cdc97755f483b5b3aa82
Corentin Couzigou <80675299+CorentinCouzigou@users.noreply.github.com>
2021-12-23 15:03:50 +0100

    environnement variables CHECK

--- app/database.js
+++ app/database.js
@@ -4,11 +4,12 @@ const sequelize = new Sequelize(process.env.PG_URL, {
     define: {
         dialect: 'postgres',
         timestamps: false,
-        dialectOptions: {
-            require: true,
-            rejectUnauthorized: false
-        }
+  
     },
+    dialectOptions: {
+        require: true,
+        rejectUnauthorized: false
+    }
 
 });
 
