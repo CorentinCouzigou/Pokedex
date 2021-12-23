b975b82cc8c6c4145513433041947eff04d1cdf2
Corentin Couzigou <80675299+CorentinCouzigou@users.noreply.github.com>
2021-12-23 14:59:24 +0100

    environnement variables CHECK

--- app/database.js
+++ app/database.js
@@ -4,7 +4,21 @@ const sequelize = new Sequelize(process.env.PG_URL, {
     define: {
         dialect: 'postgres',
         timestamps: false,
+    },
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
     }
 });
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
 
 module.exports = sequelize;
